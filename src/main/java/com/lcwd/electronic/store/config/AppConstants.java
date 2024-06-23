1dcbc4e5204b25a2bcff7e40696c50a13dbe9393
DugeshCoder <aws8896305@gmail.com>
2024-06-23 21:23:20 +0530

    changing jwt auth filter to functional style of coding

--- src/main/java/com/lcwd/electronic/store/config/AppConstants.java
+++ src/main/java/com/lcwd/electronic/store/config/AppConstants.java
@@ -4,4 +4,5 @@ public class AppConstants {
 
     public static final String ROLE_ADMIN = "ADMIN";
     public static final String ROLE_NORMAL = "NORMAL";
+    public static final String JWT_HEADER_NAME = "Authorization";
 }
